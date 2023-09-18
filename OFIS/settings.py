ffdf19b4f31a0eb49dcb58bec7b6f46e869e62cb
Long Nguyen <116535476+nhlong2510@users.noreply.github.com>
2023-09-18 20:10:11 +0700

    Update settings.py

--- OFIS/settings.py
+++ OFIS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lf)$0903olggmm)+!_&t1$a8ru)@qht-a$=exim5z=*j$lppgb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
